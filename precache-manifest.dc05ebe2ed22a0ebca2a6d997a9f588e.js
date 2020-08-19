81c2daa9fbfd801d74cbd257935fd33f044b3bce
Joblyn <oaikhenahjob@gmail.com>
2020-08-19 02:06:23 +0100

    Updates

---
+++ precache-manifest.dc05ebe2ed22a0ebca2a6d997a9f588e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704407d748c411938a4f97431b715af4",
+    "url": "/quo/index.html"
+  },
+  {
+    "revision": "0a2db4b0a04576cdae4e",
+    "url": "/quo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0ac237d4442623d47fc3",
+    "url": "/quo/static/css/main.9d35c21d.chunk.css"
+  },
+  {
+    "revision": "0a2db4b0a04576cdae4e",
+    "url": "/quo/static/js/2.f0c25e32.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/quo/static/js/2.f0c25e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac237d4442623d47fc3",
+    "url": "/quo/static/js/main.5e497b52.chunk.js"
+  },
+  {
+    "revision": "1d0adefbe21e4a30d545",
+    "url": "/quo/static/js/runtime-main.ec4ff420.js"
+  }
+]);
